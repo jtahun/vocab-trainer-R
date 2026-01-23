92bb52cdf033866fc82189c00e4db81cc6a75502
igor.zhuley <jtahun@bk.ru>
2026-01-23 14:53:18 +0500

    quant To Transkrib2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vocab-json-v7.012'; // смени версию, чтобы sw обновился
+const CACHE = 'vocab-json-v7.013'; // смени версию, чтобы sw обновился
 const ASSETS = [
   './',
   './index.html',
