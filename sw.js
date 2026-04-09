70504ba7ea76e0632412facc7a98b43498bc6c78
Igor <jtahun@bk.ru>
2026-04-09 23:19:39 +0500

    added words Bob Hadgehog

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vocab-json-v7.015'; // смени версию, чтобы sw обновился
+const CACHE = 'vocab-json-v7.016'; // смени версию, чтобы sw обновился
 const ASSETS = [
   './',
   './index.html',
